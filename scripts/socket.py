642be46ac8fcc56e882499f643f85cc6768d4c43
Azeem Mirza <azeem.mirza94@gmail.com>
2024-04-24 14:50:37 +0500

    updated socket program

--- scripts/socket.py
+++ scripts/socket.py
@@ -1,7 +1,8 @@
 import socket
 
-mysock = misc.socket(socket.AF_INET, socket.SOCK_STREAM)
+mysock = socket(socket.AF_INET, socket.SOCK_STREAM)
 mysock.connect(('data.pr4e.org', 80))
+
 # cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\r\n\r\n'.encode()
 
 cmd = 'GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n'.encode()
