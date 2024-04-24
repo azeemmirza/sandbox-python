062ad206668b100f861d536cdacf21f2173c5d99
Azeem Mirza <azeem.mirza94@gmail.com>
2024-04-24 14:05:27 +0500

    comment update

--- scripts/duplicate-elements-array.py
+++ scripts/duplicate-elements-array.py
@@ -1,13 +1,13 @@
-"""
- * Array reverse or reverse a array means changing the position of each number of the given array to its opposite position
- * from end, i.e. if a number is at position 1 then its new position will be Array.length,
- * similarly if a number is at position 2 then its new position will be Array.length – 1, and so on.
- *
- * Input: Arr = [1, 2, 3]
- * Output: ArrRev = [3, 2, 1]
- *
- * Online Editor: https://www.programiz.com/python-programming/online-compiler/
-"""
+'''
+Array reverse or reverse a array means changing the position of each number of the given array to its opposite position
+from end, i.e. if a number is at position 1 then its new position will be Array.length,
+similarly if a number is at position 2 then its new position will be Array.length - 1, and so on.
+
+Input: Arr = [1, 2, 3]
+Output: ArrRev = [3, 2, 1]
+
+Online Editor: https://www.programiz.com/python-programming/online-compiler/
+'''
 
 
 def reverse_array(arr):
